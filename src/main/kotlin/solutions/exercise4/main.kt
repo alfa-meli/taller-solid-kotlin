0a0bc0c8bae7fb809570701ecb3b13905282c56b
Alfa11111 <alvaro.mogollon@mercadolibre.com.co>
2023-03-28 19:08:36 -0500

    solid

--- src/main/kotlin/solutions/exercise4/main.kt
+++ src/main/kotlin/solutions/exercise4/main.kt
@@ -1,81 +1,52 @@
 package solutions.exercise4
 
+interface Insertable {
+    fun Insertar()
+}
+
+interface Consultable {
+    fun Consultar()
+}
+
 interface Database {
-    fun conectar()
-    fun desconectar()
-    fun insertar()
-    fun eliminar()
-    fun actualizar()
-    fun consultar()
+    fun Conectar()
+    fun Desconectar()
 }
 
 class MiBaseDeDatos : Database {
-    override fun conectar() {
+    override fun Conectar() {
         // lógica de conexión a la base de datos
     }
 
-    override fun desconectar() {
+    override fun Desconectar() {
         // lógica de desconexión de la base de datos
-    }
-
-    override fun insertar() {
-        // lógica de inserción en la base de datos
-    }
-
-    override fun eliminar() {
-        // lógica de eliminación en la base de datos
-    }
-
-    override fun actualizar() {
-        // lógica de actualización en la base de datos
-    }
-
-    override fun consultar() {
-        // lógica de consulta en la base de datos
     }
 }
 
 class Cliente {
-    fun utilizarBaseDeDatos(db: Database) {
-        // lógica de uso de la base de datos
-        db.conectar()
-        db.insertar()
-        db.consultar()
-        db.desconectar()
+    fun Insertar(db: Insertable) {
+        db.Insertar()
+    }
+
+    fun Consultar(db: Consultable) {
+        db.Consultar()
     }
 }
 
-class MiTabla : Database {
-    override fun conectar() {
-        // no implementado
-    }
-
-    override fun desconectar() {
-        // no implementado
-    }
-
-    override fun insertar() {
+class MiTabla : Insertable, Consultable {
+    override fun Insertar() {
         // lógica de inserción en la tabla
     }
 
-    override fun eliminar() {
-        // no implementado
-    }
-
-    override fun actualizar() {
-        // no implementado
-    }
-
-    override fun consultar() {
+    override fun Consultar() {
         // lógica de consulta en la tabla
     }
 }
 
 fun main() {
-    val db = MiBaseDeDatos()
     val tabla = MiTabla()
 
     val cliente = Cliente()
-    cliente.utilizarBaseDeDatos(db)
-    cliente.utilizarBaseDeDatos(tabla)
+    cliente.Insertar(tabla)
+    cliente.Consultar(tabla)
 }
